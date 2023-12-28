a8270af5018d565717dcdc1cdb31293a7deade74
Ansh Vaswani <122345274+AnSH190@users.noreply.github.com>
2023-12-28 14:01:49 +0530

    removed comments

--- src/App.js
+++ src/App.js
@@ -18,14 +18,6 @@ function App() {
   const [email, setEmail] = useState("");
   const [pass, setPass] = useState("");
   const [selectedUser, setSelectedUser] = useState(null);
-  // const Enroll = 1514;
-  // const Email = "a@gm";
-  // const Pass = 1234;
-  // const logs1 = {e: 1514, em: "a@gm", p: 4000};
-  // const logs2 = {e: 1515, em: "b@gm", p: 5000};
-  // const logs3 = {e: 1516, em: "c@gm", p: 6000};
-  // const logs4 = {e: 1517, em: "d@gm", p: 7000};
-  // const logs5 = {e: 1518, em: "e@gm", p: 8000};
 
   const logs1 = {
     e: 2204030101514,
@@ -144,7 +136,6 @@ function App() {
   };
 
   const handleReset = () => {
-    // Clear the values of enrollment, email, and password fields
     setEnroll("");
     setEmail("");
     setPass("");
@@ -153,36 +144,20 @@ function App() {
   const [showWarning, setShowWarning] = useState(false);
   const loginData = [logs1, logs2, logs3, logs4, logs5];
 
-  // useEffect(() => {
-  //   // Check if the page was reloaded and action is "Sign Up"
-  //   const isReloaded =
-  //     window.performance && window.performance.navigation.type === 1;
-  //   if (isReloaded && action === "Sign Up") {
-  //     setShowWarning(true);
-  //     setTimeout(() => {
-  //       setShowWarning(false);
-  //     }, 3000);
-  //   }
-  // }, [action]);
-
   useEffect(() => {
-    // Check if the page was reloaded and action is "Sign Up"
     const isReloaded =
       window.performance && window.performance.navigation.type === 1;
     if (isReloaded && action === "Sign Up") {
       setShowWarning(true);
 
-      // Set a timeout to hide the warning after 4 seconds
       const hideWarningTimeout = setTimeout(() => {
         setShowWarning(false);
       }, 4000);
 
-      // Set another timeout to reload the page after 4 seconds
       const reloadPageTimeout = setTimeout(() => {
         window.location.reload();
       }, 4000);
 
-      // Clean up the timeouts when the component unmounts
       return () => {
         clearTimeout(hideWarningTimeout);
         clearTimeout(reloadPageTimeout);
@@ -191,7 +166,6 @@ function App() {
   }, [action]);
 
   const validateLogin = () => {
-    // Loop through the login data objects and check if any match the input values
     for (const data of loginData) {
       if (data.e == enroll && data.p == pass && data.em == email) {
         return true;
@@ -201,7 +175,6 @@ function App() {
   };
 
   useEffect(() => {
-    // When the user logs in, find and set the selectedUser
     if (action === "Log In" && validateLogin()) {
       const user = loginData.find(
         (data) => data.e == enroll && data.p == pass && data.em == email
@@ -212,24 +185,14 @@ function App() {
 
   return (
     <div className={action === "Log In" ? "n" : "m"}>
-      {/* {logs.e == enroll &&
-      logs.p == pass &&
-      logs.em == email &&
-      action === "Log In" ? ( */}
-
       {action === "Log In" && selectedUser && validateLogin() ? (
         <Mini selectedUser={selectedUser} results={selectedUser.results} />
       ) : (
         <div className="container">
           <div className="header">
-            {/* <div className="text"> Log In</div>
-            <div className="line"></div> */}
             <img src={logo} alt="" />
           </div>
           <div className="inputs">
-            {/* {action === "Log In" ? (
-              <div></div>
-            ) : ( */}
             <div className="input">
               <img src={user_icon} alt="" />
               <input
@@ -239,20 +202,6 @@ function App() {
                 onChange={(e) => setEnroll(e.target.value)}
               />
             </div>
-            {/* <div class="form__group field">
-              <input
-                type="input"
-                class="form__field"
-                placeholder="Name"
-                required=""
-                value={enroll}
-                onChange={(e) => setEnroll(e.target.value)}
-              />
-              <label for="name" class="form__label">
-                Enrollment
-              </label>
-            </div> */}
-            {/* )} */}
             <div className="input">
               <img src={emial_icon} alt="" />
               <input
@@ -262,19 +211,6 @@ function App() {
                 onChange={(e) => setEmail(e.target.value)}
               />
             </div>
-            {/* <div class="form__group field">
-              <input
-                type="input"
-                class="form__field"
-                placeholder="Name"
-                required=""
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <label for="name" class="form__label">
-                E-Mail
-              </label>
-            </div> */}
             <div className="input">
               <img src={password_icon} alt="" />
               <input
@@ -284,43 +220,16 @@ function App() {
                 onChange={(p) => setPass(p.target.value)}
               />
             </div>
-            {/* <div class="form__group field">
-              <input
-                type="password"
-                class="form__field"
-                placeholder="Name"
-                required=""
-                value={pass}
-                onChange={(p) => setPass(p.target.value)}
-              />
-              <label for="name" class="form__label">
-                Password
-              </label>
-            </div> */}
           </div>
-          {/* {action === "Sign Up" ? (
-            <div></div>
-          ) : ( */}
           <div className="forgot">
             Forgot Password <span>Click Here</span>
           </div>
-          {/* )} */}
-          {/* <div>{enroll}</div>
-          <div> {email} </div>
-          <div> {pass} </div> */}
-          {/* {action === "Sign Up" ? <div>Enter Corrct info </div> : <div></div>} */}
           <div className="submit-con">
             <div
-              // className={action === "Sign Up" ? "submit gray" : "submit"}
               className={
-                // logs.e == enroll && logs.p == pass && logs.em == email
                 validateLogin() ? "submit" : "submit gray"
               }
-              // onClick={() => {
-              //   setAction("Log In");
-              // }}
               onClick={
-                // logs.e == enroll && logs.p == pass && logs.em == email
                 validateLogin()
                   ? () => {
                       setAction("Log In");
